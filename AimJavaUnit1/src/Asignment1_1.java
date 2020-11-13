662672e9854b546e52a1ada886adf1389902e6be
almazing <63121878+almazing@users.noreply.github.com>
2020-11-13 09:21:20 -0600

    Add files via upload

---
+++ AimJavaUnit1/src/Asignment1_1.java
@@ -0,0 +1,9 @@
+class Asignment1_1{
+    public static void main(String[] args) {
+        System.out.println("Alex Mares");
+        System.out.println("Java Web");
+        System.out.println("Omaha, NE");
+        System.out.println("Bread Pudding");
+
+    }
+}
